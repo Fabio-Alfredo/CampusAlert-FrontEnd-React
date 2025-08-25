c01a876b65d4e93375b6ca63bcabeb5de0c56e40
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-25 12:20:49 -0600

    :sparkles: feat: add new navegation in pages

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import AuthLayout from "../organisms/AuthLayout";
 import LoginForm from "../molecules/LoginForm";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const LoginTemplate = () => {
   return (
-    <AuthLayout title="Inicio de Sesion">
+    <AuthLayout title="Inicio de Sesión">
       <LoginForm />
+      <Box mt={2}>
+        <p className="text-center">
+          ¿Aún no tienes una cuenta con nosotros?
+          <Link className="text-blue-500 underline">
+            Registrarse
+          </Link>
+        </p>
+      </Box>
     </AuthLayout>
   )
 }
--- src/components/templates/RegisterTemplate.jsx
+++ src/components/templates/RegisterTemplate.jsx
@@ -6,6 +6,14 @@ const RegisterTemplate = () => {
   return (
     <AuthLayout title="Registro de Usuarios">
       <RegisterForm />
+      <Box mt={2}>
+        <p className="text-center">
+          ¿Ya tienes una cuenta con nosotros?
+          <Link className="text-blue-500 underline">
+            Iniciar Sesión
+          </Link>
+        </p>
+      </Box>
     </AuthLayout>
   )
 }
